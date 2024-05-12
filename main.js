7c0e6f09d534739d51b668aa6e299b2b68e7d4bd
Fúlvio Leo <fulviokaldas@outlook.com>
2024-05-12 13:04:21 -0300

    Ident to match my preferences

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const canvas=document.getElementById("myCanvas");
-canvas.width=200;
+const canvas = document.getElementById("myCanvas");
+canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car=new Car(100,100,30,50);
+const car = new Car(100, 100, 30, 50);
 
 animate();
 
-function animate(){
-    car.update();
+function animate() {
+  car.update();
 
-    canvas.height=window.innerHeight;
-    car.draw(ctx);
-    requestAnimationFrame(animate);
+  canvas.height = window.innerHeight;
+  car.draw(ctx);
+  requestAnimationFrame(animate);
 }
